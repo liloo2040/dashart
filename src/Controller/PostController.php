a3dd7534fd6df1b493ca4dfff2b1efcb2b1b6d06
liloo2040 <e.dufour.89@gmail.com>
2018-04-26 18:26:50 +0200

    adding twig and controller

---
+++ src/Controller/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PostController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    
+    public function index() 
+    {
+        // return new Response('Hello!');
+
+        return $this->render('posts/index.html.twig');
+    }
+}
