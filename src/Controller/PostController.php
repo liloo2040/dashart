dfc4c4bf75f964995d0adabbaf65d81e1499889d
liloo2040 <e.dufour.89@gmail.com>
2018-04-28 18:50:09 +0200

    adding form in controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,9 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PostController extends Controller
 {
@@ -28,8 +34,32 @@ class PostController extends Controller
     public function show($id) 
     {
         $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
-        
+
         return $this->render('posts/show.html.twig', array('post' => $post));
+    }
+
+    /**
+     * @Route("/post/new", name="new_post")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class, array('attr' => 
+            array('class' => 'form-control')))
+            ->add('content', TextareaType::class, array(
+                'required' => false, 
+                'attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Save',
+                'attr' => array('class' => 'btn btn-primary mt-3' )
+                ))
+            ->getForm();
+
+        return $this->render('posts/new;html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
 //    /**
