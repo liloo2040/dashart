790bc5669f2076c4a5aaf8014e6faf8c0f5ce5e8
liloo2040 <e.dufour.89@gmail.com>
2018-04-28 18:04:16 +0200

    adding new actions

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -19,6 +19,30 @@ class PostController extends Controller
     {
         $posts = $this->getDoctrine()->getRepository(Post::Class)->findAll();
 
-        return $this->render('posts/index.html.twig');
+        return $this->render('posts/index.html.twig', array('posts' => $posts));
     }
+
+    /**
+     * @Route("/post/show/{id}", name="post_show")
+     */
+    public function show($id) 
+    {
+
+    }
+
+//    /**
+//      * @Route("/post/save")
+//      */
+//     public function save()
+//     {
+//         $entityManager = $this->getDoctrine()->getManager();
+//         $post = new Post();
+//         $post->setTitle('Post Two');
+//         $post->setContent('Post Content');
+
+//         $entityManager->persist($post);
+//         $entityManager->flush();
+
+//         return new Response ('Post saved with id '.$post->getId());
+//     } 
 }
